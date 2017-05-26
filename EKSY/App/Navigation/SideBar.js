5c9839fb50b9f354c5aae64d278c13a6db23dc25
Juuso Lassila <juuso.lassila@helsinki.fi>
2017-05-26 15:34:51 +0300

    Logo added

--- EKSY/App/Navigation/SideBar.js
+++ EKSY/App/Navigation/SideBar.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native'
+import Style from './Styles/SideBarStyles'
+import {Container, Content} from 'native-base'
+import {Image, ListView} from 'react-native'
 
 class SideBar extends Component {
   render() {
     return (
-      <View>
-        <Text>sidebar</Text>
-      </View>
+      <Container>
+        <Content style={Style.content}>
+          <Image source={require('../Images/logoPlaceHolder.png')}/>
+          {/* <ListView>
+
+          </ListView> */}
+        </Content>
+      </Container>
     )
   }
 }
---
+++ EKSY/App/Navigation/Styles/SideBarStyles.js
@@ -0,0 +1,13 @@
+
+
+export default {
+
+  content: {
+    backgroundColor: "white",
+  },
+
+  image: {
+    alignSelf: 'stretch'
+  }
+
+}
