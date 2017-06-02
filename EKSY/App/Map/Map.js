fe9230764179c269d8efb74b3c8b6a595a677856
Mikko Pyykkö <mipyykko@gmail.com>
2017-06-02 12:46:01 +0300

    spinner

--- EKSY/App/Map/Map.js
+++ EKSY/App/Map/Map.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   StyleSheet,         // CSS-like styles
   Text,               // Renders text
   TouchableOpacity,   // Pressable container
-  View                // Container component
-} from 'react-native';
-import MapView from 'react-native-maps';
-import Callout from './Callout';
+  View,               // Container component
+  ActivityIndicator
+} from 'react-native'
+import MapView from 'react-native-maps'
+import Callout from './Callout'
 import styles from './Styles/MapStyles'
 import MapManager from './MapManager'
 
@@ -14,27 +15,31 @@ import testData from '../includes/data/Sarjakuvat.json'
 
 export default class Map extends Component {
 
-  constructor(props) {
+  constructor (props) {
     super(props)
-    this._manager = new MapManager();
-    this._manager.setMapObject(this);
-    this._map = null;
+    this._manager = new MapManager()
+    this._manager.setMapObject(this)
+    this._map = null
+    this.state = { mapLoaded: false }
   }
 
-  update() {
-    this.forceUpdate();
+  update () {
+    this.forceUpdate()
   }
 
-  animateToCoordinate(position, delay) {
-    if (this._map) {
-      this._map.animateToCoordinate(position, delay);
+  componentDidMount () {
+    this.setState({ mapLoaded: true })
+  }
+
+  animateToCoordinate (position, delay) {
+    if (this.state.mapLoaded) {
+      this._map.animateToCoordinate(position, delay)
     } else {
-      console.warn("Map not rendered when used animateToCoordinate");
+      console.warn('Map not rendered when used animateToCoordinate')
     }
-
   }
 
-  renderPampylat() {
+  renderPampylat () {
     return testData.features.map((pampyla, index) => {
       console.log(pampyla.properties.gx_media_links)
       return (
@@ -42,7 +47,7 @@ export default class Map extends Component {
         <MapView.Marker
           coordinate={{
             longitude: pampyla.geometry.coordinates[0],
-            latitude: pampyla.geometry.coordinates[1],
+            latitude: pampyla.geometry.coordinates[1]
           }}
           // Callout offset
           calloutOffset={{ x: -8, y: 28 }}
@@ -50,7 +55,7 @@ export default class Map extends Component {
           pinColor={'#009688'}
           key={index}
         >
-        {/* Callout */}
+          {/* Callout */}
           <MapView.Callout tooltip style={styles.callout}>
             <Callout
               name={pampyla.properties.Name}
@@ -63,23 +68,35 @@ export default class Map extends Component {
     })
   }
 
-  render() {
+  renderMapView () {
+    if (!this.state.mapLoaded) {
+      return (
+        <ActivityIndicator size='large' />
+      )
+    }
+
+    return (
+      <MapView
+        ref={(ref) => this._map = ref}
+        style={styles.map}
+        initialRegion={{
+          latitude: 60.184356,
+          longitude: 24.949326,
+          latitudeDelta: 0.0491,
+          longitudeDelta: 0.0375
+        }}
+        >
+        {this._manager.getMarkers().map((marker, index) => marker.getComponent())}
+        {this.renderPampylat()}
+      </MapView>
+    )
+  }
+
+  render () {
     return (
       <View style={styles.container}>
-        <MapView
-          ref={(ref) => this._map = ref}
-          style={styles.map}
-          initialRegion={{
-            latitude: 60.184356,
-            longitude: 24.949326,
-            latitudeDelta: 0.0491,
-            longitudeDelta: 0.0375,
-          }}
-        >
-          {this._manager.getMarkers().map((marker, index) => marker.getComponent())}
-          {this.renderPampylat()}
-        </MapView>
+        {this.renderMapView()}
       </View>
-    );
+    )
   }
 }
