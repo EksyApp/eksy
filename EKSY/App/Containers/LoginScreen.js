dd50c2062c282f5a83b92a2ba0d127443b2cf530
RIR <raine.rantanen@helsinki.fi>
2017-06-09 14:30:06 +0300

    uus LoginScreenin tynkä

--- EKSY/App/Containers/LoginScreen.js
+++ EKSY/App/Containers/LoginScreen.js
@@ -1,27 +1,23 @@
 /**
- * @class Login
- */
+* @class Login
+*/
 
 import {
-    AppRegistry,
-    TextInput,
-    Text,
-    View,
-    StyleSheet,
-    dismissKeyboard,
-    TouchableWithoutFeedback
+  Text,
+  View,
+  StyleSheet,
+  dismissKeyboard,
+  TouchableWithoutFeedback
 } from 'react-native'
 
 import MenuButton from '../Components/MenuButton'
+import Button from '../Components/Button'
 import React, {Component} from 'react'
 import * as firebase from 'firebase'
-// import {Button, Container, Header, Content, Left, Body, Title, Form, Input, Item} from 'native-base'
-import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome'
-import {Sae} from 'react-native-textinput-effects'
 import DismissKeyboard from 'dismissKeyboard'
 import PostOffice from '../lib/PostOffice'
-
-import styles from './Styles/LoginScreenStyles'
+import { FormLabel, FormInput } from 'react-native-elements'
+import Header from '../Components/Header'
 
 class LoginScreen extends Component {
   constructor (props) {
@@ -75,66 +71,45 @@ class LoginScreen extends Component {
   render () {
     return (
 
-      // <Container>
-      //   <Header>
-      //     <MenuButton />
-      //     <Left />
-      //     <Body>
-      //       <Title>Login</Title>
-      //     </Body>
-      //   </Header>
-      //   <Content>
-      //     <Form style={style.form}>
-      //       <Item>
-      //         <Input
-      //           style={style.input}
-      //           placeholder='Email'
-      //           onChangeText={(text) => this.setState({email: text})}
-      //             />
-      //       </Item>
-      //       <Item>
-      //         <Input
-      //           style={style.input}
-      //           placeholder='Password'
-      //           onChangeText={(text) => this.setState({password: text})}
-      //           secureTextEntry
-      //             />
-      //       </Item>
-      //     </Form>
-      //     <View style={style.buttons}>
-      //       <Button style={style.button} block onPress={() => this.login()}>
-      //         <Text style={style.text}>Login</Text>
-      //       </Button>
-      //       <Button style={style.button} block onPress={() => this.signup()}>
-      //         <Text style={style.text}>Signup</Text>
-      //       </Button>
-      //     </View>
-      //     <View style={style.response}>
-      //       <Text style={style.responseText}>{this.state.response}</Text>
-      //     </View>
-      //   </Content>
-      // </Container>
-    
       <View>
-        <Text>LoginScreen</Text>
+        <Header />
+        <View>
+          <FormLabel>Email</FormLabel>
+          <FormInput
+            onChangeText={(text) => this.setState({email: text})}
+          />
+        </View>
+
+        <View>
+          <FormLabel>Password</FormLabel>
+          <FormInput
+            onChangeText={(text) => this.setState({password: text})}
+            secureTextEntry
+          />
+        </View>
+        <View style={styles.buttons}>
+          <Button onPress={() => this.login()}>
+          Log in
+          </Button>
+          <Button onPress={() => this.signup()}>
+          Sign up
+          </Button>
+        </View>
+        <View style={styles.response}>
+          <Text style={styles.responseText}>{this.state.response}</Text>
+        </View>
       </View>
     )
   }
 }
 
-const style = {
+const styles = StyleSheet.create({
   input: {
     marginTop: 20
   },
 
   buttons: {
     marginTop: 50
-  },
-
-  button: {
-    marginTop: 20,
-    width: '50%',
-    alignSelf: 'center'
   },
 
   text: {
@@ -155,6 +130,6 @@ const style = {
     marginTop: 50
   }
 
-}
+})
 
 export default LoginScreen
