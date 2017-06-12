5502fd8e3a7509be1640faf89f15c83abf6cec2b
RIR <raine.rantanen@helsinki.fi>
2017-06-12 14:25:38 +0300

    tyyli yhtenäinen

--- EKSY/App/Containers/AddMarker.js
+++ EKSY/App/Containers/AddMarker.js
@@ -8,35 +8,34 @@ import Header from '../Components/Header'
 import * as ReduxActions from '../Actions'
 import {Actions} from 'react-native-router-flux'
 import {connect} from 'react-redux'
-import {backgroundColor, frontgroundColor} from '../Theme'
-import {Grid, Row, FormInput, Button} from 'react-native-elements'
-import ActionButton from '../Components/ActionButton'
+import * as Theme from '../Theme'
+import {Grid, Row, FormInput} from 'react-native-elements'
+import Button from '../Components/Button'
 
 class AddMarker extends Component {
-	
-	constructor(props) {
-		super(props)
-		
-		this.state = {
-			text: ""
-		}
-		
-		this.mapManager = new MapManager();
-	}
-	
-	addNewMarker() {
-		let latitude = this.props.currentRegion.latitude;
-		let longitude = this.props.currentRegion.longitude;
-		let marker = <Marker latitude={latitude} longitude={longitude} text={this.state.text}/>;
-		this.mapManager.addMarker(marker);
-		setTimeout(() => this.mapManager.flyToPosition(latitude, longitude), 1000)
-		Actions.mapView();
+
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      text: ''
+    }
+
+    this.mapManager = new MapManager()
+  }
+
+  addNewMarker () {
+    let latitude = this.props.currentRegion.latitude
+    let longitude = this.props.currentRegion.longitude
+    let marker = <Marker latitude={latitude} longitude={longitude} text={this.state.text} />
+    this.mapManager.addMarker(marker)
+    setTimeout(() => this.mapManager.flyToPosition(latitude, longitude), 1000)
+    Actions.mapView()
 		// this.mapManager.update();
-	}
-	
-	
-	render() {
-		return (
+  }
+
+  render () {
+    return (
 				// <Container>
 				//   <Header>
 				//
@@ -60,69 +59,68 @@ class AddMarker extends Component {
 				//   </Content>
 				//
 				// </Container>
-				
-				<View style={styles.container}>
-					<Header title="Add Marker" menuButtonPress={this.props.menuButtonPress}/>
-					<Grid>
-						<Row size={2} containerStyle={styles.row}>
-							<PointSelector onChange={(region) => {
-								this.props.regionChange(region)
-							}} currentRegion={this.props.currentRegion} style={styles.map}/>
-						</Row>
-						<Row size={2} containerStyle={styles.row}>
-							<FormInput onChangeText={(text) => this.setState({text: text})} multiline numberOfLines={8} style={styles.textArea}/>
-						</Row>
-						<Row size={1} containerStyle={styles.row}>
-							<ActionButton title="Add" onPress={() => {
-								this.addNewMarker()
-							}}/>
-						</Row>
-					</Grid>
-				</View>
+
+      <View style={styles.container}>
+        <Header title='Add Marker' menuButtonPress={this.props.menuButtonPress} />
+        <Grid>
+          <Row size={2} containerStyle={styles.row}>
+            <PointSelector onChange={(region) => {
+              this.props.regionChange(region)
+            }} currentRegion={this.props.currentRegion} style={styles.map} />
+          </Row>
+          <Row size={2} containerStyle={styles.row}>
+            <FormInput onChangeText={(text) => this.setState({text: text})} multiline numberOfLines={5} style={styles.textArea} />
+          </Row>
+          <Row size={1} containerStyle={styles.row}>
+            <Button onPress={() => {this.addNewMarker()}}>
+							Add
+						</Button>
+          </Row>
+        </Grid>
+      </View>
 		)
-	}
+  }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		backgroundColor: backgroundColor,
-		flex: 1
-	},
-	
-	row: {
-		width: '100%'
-	},
-	
-	map: {
-		flex: 1,
-	},
-	
-	textArea: {
-		flex: 1,
-		textAlignVertical: 'top',
-		width: '100%',
-		backgroundColor: frontgroundColor,
-		marginTop: 10
-	}
-	
-	
+  container: {
+    backgroundColor: Theme.backgroundColor,
+    flex: 1
+  },
+
+  row: {
+    width: '100%'
+  },
+
+  map: {
+    flex: 1
+  },
+
+  textArea: {
+    flex: 1,
+    textAlignVertical: 'top',
+    width: '100%',
+    backgroundColor: Theme.inputColor,
+    marginTop: 20
+  }
+
 })
 
 const mapStateToProps = (state) => {
-	return {
-		currentRegion: state.map.currentRegion
-	}
+  return {
+    currentRegion: state.map.currentRegion
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
-	return {
-		menuButtonPress: () => {
-			dispatch(ReduxActions.drawerOpen())
-		},
-		regionChange: (region) => {
-			dispatch(ReduxActions.updateRegion(region))
-		}
-	}
+  return {
+    menuButtonPress: () => {
+      dispatch(ReduxActions.drawerOpen())
+    },
+    regionChange: (region) => {
+      dispatch(ReduxActions.updateRegion(region))
+    }
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(AddMarker)
