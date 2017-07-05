d122707a5c6be147e9586493fd6f811bb07ffeb9
Juuso Lassila <juuso.lassila@helsinki.fi>
2017-07-05 12:47:27 +0300

    bug fix

--- EKSY/App/Dao/FirebaseDao.js
+++ EKSY/App/Dao/FirebaseDao.js
@@ -71,12 +71,15 @@ class FirebaseDao {
 		let markers = await firebase.database().ref("/markers/markers_info")
 		let markerRef = await markers.push()
 		let key = markerRef.key
+		await markerRef.set(marker)
 		this._addGeofireLocation(key, marker.latitude, marker.longitude)
 		this._addMarkerToCurrentUser(key)
 		if (marker.images.length > 0) {
 			marker.images = await this._uploadImages(key, marker.images)
+			imageRef = await firebase.database().ref("/markers/markers_info/" + key + "/images")
+			imageRef.set(marker.images);
 		}
-		markerRef.set(marker)
+		
 	}
 
 	async _addInfoToMarker(marker) {
