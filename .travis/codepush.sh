a91a980f7c9e160623a631567ad974d01f74ac90
Pyykkö Mikko P <pyykkomi@dat-10.cs.helsinki.fi>
2017-08-16 13:13:07 +0300

    let's see

--- .travis/codepush.sh
+++ .travis/codepush.sh
@@ -2,5 +2,9 @@
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     cd android && ./gradlew assembleRelease
-    code-push-travis -d Production --platform ["android"] --targetBinary `cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]'`
+    cd ..
+    code-push login --accessKey $CODE_PUSH_ACCESS_KEY
+    code-push release-react EKSY android --development Production --entryFile index.android.js --targetBinaryVersion "*"
+    code-push logout
+    #code-push-travis -d Production --platform ["android"] --targetBinary
 fi
