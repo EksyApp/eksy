b6b41c6da230d0a50616fde506b8ca70882ebb25
Pyykkö Mikko P <pyykkomi@dat-10.cs.helsinki.fi>
2017-08-18 13:23:51 +0300

    fixed keystore.enc location and package name

--- .travis/assembleRelease.sh
+++ .travis/assembleRelease.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-    cp $TRAVIS_BUILD_DIR/my-release-key.keystore android/app/
     cd android && ./gradlew assembleRelease
     cd ..
     code-push login --accessKey $CODE_PUSH_ACCESS_KEY
--- EKSY/android/app/src/main/java/com/eksy/SplashActivity.java
+++ EKSY/android/app/src/main/java/com/eksy/SplashActivity.java
@@ -1,4 +1,4 @@
-package com.eksy;
+package com.eksyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
