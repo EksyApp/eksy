b33248c546cb34b4fe0a5d1e3c562c0f16c9bfca
Pyykkö Mikko P <pyykkomi@dat-10.cs.helsinki.fi>
2017-08-21 12:30:37 +0300

    added version number to settings component

--- EKSY/App/Components/Settings/SettingsComponent.js
+++ EKSY/App/Components/Settings/SettingsComponent.js
@@ -4,25 +4,37 @@ import Header from "../Common/Header";
 import FilterSettingsContainer from "../../Containers/Settings/FilterSettingsContainer";
 import * as Theme from "../../Theme";
 import ProfileContainer from "../../Containers/Settings/ProfileContainer";
-
+import VersionNumber from "react-native-version-number"
 
 export default class SettingsComponent extends Component {
-	
+
 	render () {
 		return (
 				<View style={styles.container}>
 					<Header title='Filter Settings' backButton />
 					<ProfileContainer/>
 					<FilterSettingsContainer/>
+					<View style={styles.versionView}>
+						<Text style={styles.versionText}>{VersionNumber.appVersion}</Text>
+					</View>
 				</View>
 		)
 	}
-	
+
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: Theme.backgroundColor,
+	},
+	versionView: {
+		flex: 1,
+		flexDirection: 'row',
+		alignItems: 'flex-end',
+		margin: 15
+	},
+	versionText: {
+		fontSize: 12
 	}
 })
