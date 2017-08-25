da6310ccf7c83a4d0136968421b770ba581fe8c3
Pyykkö Mikko P <pyykkomi@dat-10.cs.helsinki.fi>
2017-08-25 12:20:13 +0300

    oh /god/

--- .travis/assembleRelease.sh
+++ .travis/assembleRelease.sh
@@ -2,8 +2,9 @@
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     cd android && ./gradlew assembleRelease
-    cd ..
+    cd fastlane
     fastlane beta --verbose
+    cd ../..
     code-push login --accessKey $CODE_PUSH_ACCESS_KEY
     code-push release-react EKSY android -d Production --entryFile index.android.js --targetBinaryVersion "*"
     code-push logout
